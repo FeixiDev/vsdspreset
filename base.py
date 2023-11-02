f3f2b3aea1051b3b8c8fe62fd7c043869b20c762
“TonyLu12138” <tony.lu@feixitek.com>
2023-11-02 08:27:16 +0000

    基础类，实现命令执行以及记录命令执行日志

--- base.py
+++ base.py
@@ -3,17 +3,17 @@
 
 import subprocess
 
+
 class Base:
     def __init__(self, logger):
         self.logger = logger
-    
+
     def com(self, command):
         try:
-            result = subprocess.run(command, stdout=subprocess.PIPE, shell=True, check=True, text=True).stdout 
+            result = subprocess.run(
+                command, stdout=subprocess.PIPE, shell=True, check=True, text=True).stdout
             self.logger.log(f"执行命令：{command}")
             return result
         except subprocess.CalledProcessError as e:
             self.logger.log(f"命令 {command} 执行失败 {e}")
             return f"命令执行失败: {str(e)}"
-    
-    
