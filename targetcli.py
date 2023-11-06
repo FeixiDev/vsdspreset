d9862674de5211665309b3d61828d7a23d8d9ec4
TonyLu12138 <138006045+TonyLu12138@users.noreply.github.com>
2023-11-06 18:28:53 +0800

    增加了check_targetcli_configuration方法日志记录的结果部分

--- targetcli.py
+++ targetcli.py
@@ -24,17 +24,19 @@ class TargetCLIConfig:
             command = f"targetcli get global {buffer}"
 
             if buffer == "auto_add_default_portal" or buffer == "auto_add_mapped_luns":
-                if "false" not in self.base.com(command):
+                if "false" not in self.base.com(command).stdout:
                     self.logger.log(f"auto_add_default_portal配置失败：{command}")
                     return False
                 else:
+                    self.logger.log(f"执行命令：{command} 结果：{self.base.com(command).stdout}")
                     print(f"检查 {buffer} 通过")
                     return True
             elif buffer == "auto_enable_tpgt":
-                if "true" not in self.base.com(command):
+                if "true" not in self.base.com(command).stdout:
                     self.logger.log(f"auto_enable_tpgt配置失败：{command}")
                     return False
                 else:
+                    self.logger.log(f"执行命令：{command} 结果：{self.base.com(command).stdout}")
                     print(f"检查 {buffer} 通过")
                     return True
             buffer = None
